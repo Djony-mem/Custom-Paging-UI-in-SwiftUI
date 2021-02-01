50153f3b514c2b8c0064553dfc66fff427536d90
Бруйко Евгения <bruyko.e@gmail.com>
2021-02-01 23:11:42 +0300

    Initial Commit

---
+++ Custom Paging UI in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Paging UI in SwiftUI
+//
+//  Created by Brubrusha on 2/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom Paging UI in SwiftUI/Custom_Paging_UI_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Paging_UI_in_SwiftUIApp.swift
+//  Custom Paging UI in SwiftUI
+//
+//  Created by Brubrusha on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Paging_UI_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
